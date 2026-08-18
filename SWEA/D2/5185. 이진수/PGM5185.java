00136bffbb5664c087f435d8f6ff61a7b5f9e264
ComelyU <demise1426@gmail.com>
2026-08-18 19:36:21 +0900

    Solved: SWEA 5185. [S/W 문제해결 구현] 1일차 - 이진수

---
+++ SWEA/D2/5185. 이진수/PGM5185.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class PGM5185 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase++) {
+            sb.append('#').append(testCase).append(' ');
+
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int N = Integer.parseInt(st.nextToken());
+            String hexadecimalNum = st.nextToken();
+
+            for (int i = 0; i < N; i++) {
+                int decimalNum = Integer.parseInt(String.valueOf(hexadecimalNum.charAt(i)), 16);
+
+                String binaryNum = String.format("%4s", Integer.toBinaryString(decimalNum)).replace(' ', '0');
+
+                sb.append(binaryNum);
+            }
+
+            sb.append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+
+}
---
+++ SWEA/D2/5185. 이진수/README.md
@@ -0,0 +1,11 @@
+# [D2] [S/W 문제해결 구현] 1일차 - 이진수 - 5185
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWTtiyIqd_wDFAVT)
+
+### 성능 요약
+
+~~메모리:  KB, 시간:  ms, 코드길이:  Bytes~~
+
+Python 제출만 가능. Java 제출 지원 X.
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
